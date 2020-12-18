d1734e0146de90acb8382f2e525c667a9d8af441
Florian Morel <ayamflow@gmail.com>
2020-12-18 14:06:53 +0100

    add query utils in lib

---
+++ src/lib/query.js
@@ -0,0 +1,17 @@
+var params = {}
+
+export function query(value) {
+    return params[value]
+}
+
+function parse(value) {
+    // TODO: call on history/page change?
+    let urlParams = new URLSearchParams(value || location.search)
+    params = Object.fromEntries(urlParams.entries())
+    Object.keys(params).forEach(key => {
+        let val = params[key]
+        if (val == '') params[key] = true
+    })
+}
+
+parse()
