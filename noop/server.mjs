7fa876034a42350283c963171c92db96a9d1f49a
Florian Morel <ayamflow@gmail.com>
2020-12-28 13:39:29 +0100

    server: use sirv instead of use-static to serve static files with "single" option (for SPA)

--- noop/server.mjs
+++ noop/server.mjs
@@ -1,12 +1,13 @@
 import polka from 'polka'
-import useStatic from 'use-static'
+import sirv from 'sirv'
 import path from 'path'
 import { log } from './log.mjs'
 import kleur from 'kleur'
 const dir = path.join(path.resolve(), './static')
 
 export function serve({ port = 3000 }) {
-    polka().use(useStatic(dir)).listen(port, error => {
+    polka()
+    .use(sirv(dir, {single: true})).listen(port, error => {
         if (error) throw error
         let localhost = `http://localhost:${port}`
         log(`${kleur.underline('Serving')} files on ${kleur.yellow(localhost)}`)
