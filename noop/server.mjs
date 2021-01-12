ecd51996f34938445f1c4a4feb0264b50d4c935d
Florian Morel <ayamflow@gmail.com>
2021-01-12 22:06:54 +0100

    server: return instance so that it can be used for other things (like EventSource)

--- noop/server.mjs
+++ noop/server.mjs
@@ -6,8 +6,8 @@ import kleur from 'kleur'
 const dir = path.join(path.resolve(), './static')
 
 export function serve({ port = 3000 }) {
-    polka()
-    .use(sirv(dir, {single: true})).listen(port, error => {
+    // return polka().use(sirv(dir, {single: true})).listen(port, error => {
+    return polka().use(sirv(dir)).listen(port, error => {
         if (error) throw error
         let localhost = `http://localhost:${port}`
         log(`${kleur.underline('Serving')} files on ${kleur.yellow(localhost)}`)
