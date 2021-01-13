49237dccfa9a2a40a2e8b11bcd16d05f1e5f5f97
Florian Morel <ayamflow@gmail.com>
2021-01-13 18:58:38 +0100

    server: use sirv opts.ignores to fix conflict between SPA mode and EventSource

--- noop/server.mjs
+++ noop/server.mjs
@@ -6,8 +6,13 @@ import kleur from 'kleur'
 const dir = path.join(path.resolve(), './static')
 
 export function serve({ port = 3000 }) {
-    // return polka().use(sirv(dir, {single: true})).listen(port, error => {
-    return polka().use(sirv(dir)).listen(port, error => {
+    const params = {
+        single: true,
+        dev: true,
+        ignores: '_dev'
+    }
+
+    return polka().use(sirv(dir, params)).listen(port, error => {
         if (error) throw error
         let localhost = `http://localhost:${port}`
         log(`${kleur.underline('Serving')} files on ${kleur.yellow(localhost)}`)
